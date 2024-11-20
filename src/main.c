637e1723528c70a66e6b2fd0ca1749ccb4a1cc55
ByScream_ <mathis.richard@sfr.fr>
2024-11-20 09:32:49 +0100

    A tester comme cela

--- src/main.c
+++ src/main.c
@@ -12,36 +12,57 @@ int main(int argc, char *argv[]) {
     // Implémentation de la logique de sauvegarde et restauration
     // Exemples : gestion des options --backup, --restore, etc.
 
-    /*--backup : crée une nouvelle sauvegarde du répertoire source, localement ou sur le serveur distant. Ne s'utilise pas avec les options --restore et --list-backups
---restore : restaure une sauvegarde à partir du chemin, localement ou depuis le serveur. Ne s'utilise pas avec les options --backup et --list-backups
---list-backups : liste toutes les sauvegardes existantes, localement ou sur le serveur. Ne s'utilise pas avec les options --restore et --backup
---dry-run : test une sauvegarde ou une restauration sans effectuer de réelles copies
---d-server : spécifie l'adresse IP du serveur à utiliser comme destination
---d-port : spécifie le port du serveur de destination
---s-server : spécifie l'adresse IP du serveur à utiliser comme source
---s-port : spécifie le port du serveur source
---dest : spécifie le chemin de destination de la sauvegarde ou de la restauration
---source : spécifie le chemin source de la sauvegarde ou de la restauration
---verbose ou v : affiche plus d'informations sur l'exécution du programme*/
 	int opt = 0;
 	struct option my_opts[] = {
-		{.name="--backup",.has_arg=0,.flag=0,.val='b'},
-		{.name="--restore",.has_arg=1,.flag=0,.val='a'},
-		{.name="--binary",.has_arg=2,.flag=0,.val='bi'},
+		{.name="backup",.has_arg=0,.flag=0,.val='b'},
+		{.name="restore",.has_arg=0,.flag=0,.val='r'},
+        {.name="list-backups",.has_arg=0,.flag=0,.val='l'},
+        {.name="dry-run",.has_arg=0,.flag=0,.val='d'},
+        {.name="d-server",.has_arg=0,.flag=0,.val='1'},
+        {.name="d-port",.has_arg=0,.flag=0,.val='2'},
+        {.name="s-server",.has_arg=0,.flag=0,.val='3'},
+        {.name="s-port",.has_arg=0,.flag=0,.val='4'},
+        {.name="dest",.has_arg=0,.flag=0,.val='5'},
+        {.name="source",.has_arg=0,.flag=0,.val='s'},
+        {.name="verbose",.has_arg=0,.flag=0,.val='v'},
 		{.name=0,.has_arg=0,.flag=0,.val=0}, // last element must be zero
 	};
-	while((opt = getopt_long(argc, argv, "", my_opts, NULL)) != -1) {
+	while((opt = getopt_long(argc, argv, "v", my_opts, NULL)) != -1) {
 		switch (opt) {
 			case 'b':
-				printf("Backup");
+				printf("--backup\n");
 				break;
 				
-			case 'a':
-				printf("restore");
+			case 'r':
+				printf("--restore\n");
 				break;
 				
-			case 'bi':
-				printf("binary");
+			case 'l':
+				printf("--list-backups\nNe s'utilise pas avec les options --restore et --backup\n");
+				break;
+            case 'd':
+				printf("--dry-run\n");
+				break;
+            case '1':
+				printf("--d-server\n");
+				break;
+            case '2':
+				printf("--d-port\n");
+				break;
+            case '3':
+				printf("--s-server\n");
+				break;
+            case '4':
+				printf("--s-port\n");
+				break;
+            case '5':
+				printf("--dest\n");
+				break;
+            case 's':
+				printf("--source\n");
+				break;
+            case 'v':
+				printf("--verbose ou -v\n");
 				break;
 		}
 	}
