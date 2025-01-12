d7774803be097b979e0b9bca0ab8765f4de79fcd
Ahmedooun <ahmed-ounissi@hotmail.com>
2025-01-12 14:59:55 +0100

    Update network.h

--- src/network.h
+++ src/network.h
@@ -10,7 +10,7 @@
 #include <string.h>
 
 void send_data(const char *server_address, int port, const void *data, size_t size);
-void receive_data(int port, void **data, size_t *size);
+size_t receive_data(int port, size_t size);
 
 
 #endif // NETWORK_H
